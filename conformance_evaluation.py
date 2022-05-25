bc01be85bfd19f87c3d5a5c3695f8bc657e0b198
Martin Bauer <martin.bauer@hu-berlin.de>
2022-05-25 13:27:35 +0200

    added standard error with finite population correction

--- conformance_evaluation.py
+++ conformance_evaluation.py
@@ -2,6 +2,7 @@ import argparse
 from math import sqrt
 
 import matplotlib.pyplot as plt
+import numpy as np
 from pm4py.algo.evaluation.replay_fitness import algorithm as evaluator
 
 from construct_alignments import deserialize_alignments
@@ -14,21 +15,26 @@ def get_log_conformance(alignments):
 def plot_fitness_distribution(traces):
     fitness_values = [x["fitness"] for x in traces]
     #print(fitness_values)
-    plt.hist(fitness_values, 100)
+    plt.hist(fitness_values, 100, weights=np.zeros_like(fitness_values) + 1. / len(fitness_values))
     plt.show()
 
 
 def estimate_standard_error(alignments):
-    sample_sizes = [100, 200, 300, 400, 500]
+    sample_sizes = [10, 20, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500]
     #get log fitness
     fitness = get_log_conformance(alignments)["log_fitness"]
     #get standard deviation
-    trace_fitness = [x["log_fitness"] for x in alignments]
-    stddev = sqrt(1/len(alignments) * sum([pow(fitness - x, 2) for x in trace_fitness]))
+    trace_fitness = [x["fitness"] for x in alignments]
+    var = 1/len(alignments) * sum([pow(fitness - x, 2) for x in trace_fitness])
 
     for n in sample_sizes:
-        std_err = stddev / sqrt(n)
-        print (f"Standard Error for sample size {n}: {std_err}")
+        std_err = sqrt(var / n)
+        print(f"Standard Error (w replacement) for sample size {n}: {std_err}")
+
+    print()
+    for n in sample_sizes:
+        std_err = sqrt(var / n) * (1- ((n-1)/(len(alignments)-1)))
+        print(f"Standard Error+finite population correction for sample size {n}: {std_err}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -37,6 +43,7 @@ if __name__ == '__main__':
 
     alignments = deserialize_alignments(args.alignments)
 
-    print(get_log_conformance(alignments)+"\n")
+    print(get_log_conformance(alignments))
+    print()
     plot_fitness_distribution(alignments)
     estimate_standard_error(alignments)
